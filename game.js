a71fc7ceaa2bb542de116e9cb70c821c91a418fe
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2020-09-15 19:29:31 +0530

    Created game.js

---
+++ game.js
@@ -0,0 +1,10 @@
+//Hello World of Phaser = Basic Game = Single scene in Spin and Win game
+//How to create a basic skeleton for the game -> Game loop.
+//creating a config object
+
+
+
+let prizes_config = {
+    count : 12,
+    prize_names : ["3000 credits","35% off","Hard Luck","70% off","swagpack","100% off","netflix","50% off","Amazon Voucher","2 extra spin","CB tshirt","CB book"] 
+}
